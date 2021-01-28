e91991f884107c9d3ee76d6f3ef8864a95028da8
josealves380 <netopereira464@gmail.com>
2021-01-27 21:02:30 -0300

    Simulando um tempo de 2 segundos para realizar a soma

--- src/views/examples/UseMemo.jsx
+++ src/views/examples/UseMemo.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 function sum(a,b) {
+    const future = Date.now() + 2000
+    while(Date.now() < future) {} //espera... 2s
     return a + b
 }
 
