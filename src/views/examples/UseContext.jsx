1225233534e07f19ccce6d3eae66c01716f883c4
josealves380 <netopereira464@gmail.com>
2021-01-29 22:31:51 -0300

    Deixando o contextAPI para toda a aplicação

--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -7,16 +7,27 @@ const UseContext = (props) => {
 
     const context = useContext(DataContext)
 
+    function addNumber(delta) {
+        context.setState({
+            ...context.state,
+            number: context.state.number + delta
+        })
+    }
+
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!" />
 
-                <div className="center">
-                    <span className="text">{context.text}</span>
-                    <span className="number">{context.number}</span>
+            <div className="center">
+                <span className="text">{context.state.text}</span>
+                <span className="text">{context.state.number}</span>
+                <div>
+                    <button className="btn" onClick={() => addNumber(-1)}>-1</button>
+                    <button className="btn" onClick={() => addNumber(1)}>+1</button>
                 </div>
+            </div>
         </div>
     )
 }
