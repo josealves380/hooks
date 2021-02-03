7110ff4edf053e449225bf314fd0e4d50b690c2f
josealves380 <netopereira464@gmail.com>
2021-02-02 21:02:39 -0300

    Adicionando hooks cutomizados

--- src/views/examples/UseReducer.jsx
+++ src/views/examples/UseReducer.jsx
@@ -1,32 +1,7 @@
 import React, { useReducer } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
-const initialState = {
-    cart: [],
-    products: [],
-    user: null,
-    //foco
-    number: 0
-}
-
-function reducer(state, action) {
-    switch (action.type) {
-        case 'numberAdd2':
-            return { ...state, number: state.number + 2 }
-        case 'numberMult7':
-            return { ...state, number: state.number * 7 }
-        case 'numberDiv25':
-            return { ...state, number: state.number / 25 }
-        case 'numberInt':
-            return { ...state, number: parseInt(state.number) }
-        case 'numberAddN':
-            return { ...state, number: state.number + action.payload }
-        case 'login':
-            return { ...state, user: { name: action.payload } }
-        default:
-            return state
-    }
-}
+import { initialState, reducer } from '../../store'
 
 const UseReducer = (props) => {
     const [state, dispatch] = useReducer(reducer, initialState)
